9e7ece6022cb284278a97e2f812c43ac712dc9af
KAVYA-AMAJALA <119871787+KAVYA-AMAJALA@users.noreply.github.com>
2024-03-23 21:25:17 +0530

    Create fibonacci series upto nth term.py

---
+++ fibonacci series upto nth term.py
@@ -0,0 +1,10 @@
+class Solution:
+    def series(self, n):
+        # Code here
+        if n==0:
+            return [0]
+        a=[0,1]
+        for i in range(2,n+1):
+            b=(a[i-1]+a[i-2])%((10**9)+7)
+            a.append(b)
+        return a
